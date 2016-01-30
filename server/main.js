2bcdaa7710ea398d89c2474b53a473e8c4600104
Ronak Gajrawala <usandfriends@gmail.com>
2016-01-30 00:20:09 -0800

    Add config and try to fix express

--- server/main.js
+++ server/main.js
@@ -1,23 +1,23 @@
+var config = require('./config');
 var shortid = require('shortid');
 
 var express = require('express');
-var server = require('http').Server(app);
 var app = express();
-
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 io.on('connection', function(socket) {
 	console.log(socket);
 });
 
+app.use(express.static(__dirname + '/../public'));
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/../views');
 
-app.use('/', function(req, res) {
+app.use('/', (req, res) => {
 	res.render('ecco');
 });
 
-app.use(express.static(__dirname + '/../public'));
-
-app.listen(3030);
-console.log('Listening!');
+server.listen(config.port);
+console.log(`Listening on port ${config.port}!`);
